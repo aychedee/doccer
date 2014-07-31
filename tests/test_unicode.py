1381789f9e2d3e1f4ca070b1c5ec90269913f618
Hansel Dunlop <hansel@interpretthis.org>
2014-07-31 11:45:26 +0100

    Add some explicit tests for unicode characters

--- tests/test_unicode.py
+++ tests/test_unicode.py
@@ -6,21 +6,21 @@ import json
 from integration import DoccerTestCase
 
 
-class ListDocsTest(DoccerTestCase):
+class UnicodeTest(DoccerTestCase):
 
-    def test_documents_can_be_listed(self):
-        name1 = 'A doccer document'
-        name2 = 'My second+'
+    def test_document_list_with_unicode(self):
+        name1 = u'☠   ⚓'
+        name2 = u'⚓⚓⚓'
         self.api('/new', 'POST', data=dict(name=name1), status_code=301)
         self.api('/new', 'POST', data=dict(name=name2), status_code=301)
 
         data = json.loads(self.api('/docs/', 'GET'))
 
-        self.assertEqual(data[0]['name'], 'A doccer document')
-        self.assertEqual(data[0]['encoded'], 'A+doccer+document')
+        self.assertEqual(data[0]['name'], name1)
+        self.assertEqual(data[0]['encoded'], u'%E2%98%A0+++%E2%9A%93')
         self.assertEqual(
             data[0]['history'][0]['hash'], self.EMPTY_HASH)
-        self.assertEqual(data[1]['name'], 'My second+')
-        self.assertEqual(data[1]['encoded'], 'My+second%2B')
+        self.assertEqual(data[1]['name'], name2)
+        self.assertEqual(data[1]['encoded'], u'%E2%9A%93%E2%9A%93%E2%9A%93')
         self.assertEqual(
             data[1]['history'][0]['hash'], self.EMPTY_HASH)
