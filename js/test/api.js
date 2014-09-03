644ce3134c5e28477cba028390dffd08156346b6
Hansel Dunlop <hansel@interpretthis.org>
2014-09-03 18:53:19 +0100

    Adds empty do nothing js tests

---
+++ js/test/api.js
@@ -0,0 +1,14 @@
+'user strict';
+
+var assert = require("assert");
+
+describe('newDoc', function() {
+
+    it('should create new doc object from server response', function() {
+
+        assert.equal(1, 1);
+
+    });
+
+});
+
